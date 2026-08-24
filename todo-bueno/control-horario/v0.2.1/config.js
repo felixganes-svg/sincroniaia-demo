2a4a613eef3ed9ef1c9ebad810548604a66cc57e
felixganes-svg <felix.ganes@gmail.com>
2026-08-24 23:05:00 +0200

    Todo Bueno control horario config v0.2.8

--- todo-bueno/control-horario/v0.2.1/config.js
+++ todo-bueno/control-horario/v0.2.1/config.js
@@ -1,4 +1,4 @@
 window.TODO_BUENO_CONFIG = {
-  VERSION: "0.2.7",
+  VERSION: "0.2.8",
   API_URL: "https://script.google.com/macros/s/AKfycbxONpdyc9QUwgfZMPBqvtG_x0ENDBDAfdbWzDKnD3ZwSW9II15oxt0Z0indN-971CzxEA/exec"
 };
