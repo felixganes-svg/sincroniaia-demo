c7cc716703a43226062a21e3a0501d591a0f1f21
felixganes-svg <felix.ganes@gmail.com>
2026-08-25 00:43:19 +0200

    Conectar backend final v0.3.6

--- todo-bueno/control-horario/config.js
+++ todo-bueno/control-horario/config.js
@@ -1,4 +1,4 @@
 window.TODO_BUENO_CONFIG = {
-  VERSION: "0.2.0",
-  API_URL: ""
+  VERSION: "0.3.6",
+  API_URL: "https://script.google.com/macros/s/AKfycbzvN72QIBEiJ6ZJ5coC8BVTvLWYqhYzIUDeGH4oZPNuZ5GdMiAuc6g2dIJ3J067b1dLFg/exec"
 };
