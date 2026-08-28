6a5cf4224e501cafdec9c88ae55b2a110adb6e1f
felixganes-svg <felix.ganes@gmail.com>
2026-08-28 17:46:38 +0200

    Add files via upload

---
+++ demo-control-horario/config.js
@@ -0,0 +1,4 @@
+window.TODO_BUENO_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbwEToPtO2mYt_j4hpZcCJzQvo2TVXPV_qvRQtSKK1QtaIy3Ci43gOYjZ1KIMCVAUFsuHw/exec',
+  VERSION: '0.4.2-DEMO-GENERICA'
+};
