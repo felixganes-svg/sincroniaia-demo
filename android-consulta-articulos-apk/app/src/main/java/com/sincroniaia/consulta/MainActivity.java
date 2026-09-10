efc7095c9b5337327d0f9988cca22d4612d954e3
felixganes-svg <felix.ganes@gmail.com>
2026-09-10 10:17:57 +0200

    Añadir aplicación Android WebView de Consulta de artículos

---
+++ android-consulta-articulos-apk/app/src/main/java/com/sincroniaia/consulta/MainActivity.java
@@ -0,0 +1,39 @@
+package com.sincroniaia.consulta;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    private static final String APP_URL = "https://felixganes-svg.github.io/sincroniaia-demo/tpv-alimentacion-consulta-articulos/?apk=1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setLoadsImagesAutomatically(true);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(APP_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
